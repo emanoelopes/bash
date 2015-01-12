46a836f006cde39985d4b67937372114d38694dc
Emanoel Lopes <emanoel@ufc.br>
2015-01-12 11:45:55 -0300

    Mudando a extensao manualmente

--- renomeia.sh
+++ renomeia.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+#Renomear arquivos com o parametro $1 seguido de uma sequencia numerica.
 
-arq=`ls`
+#arq=`ls`
 
-if [ -f "$arq" ] ; then
-a=0
-for i in *; do
-	echo "extensao ${i#*.} encontrada"
-	let a++
-done
-else
-	echo "Nenhum arquivo encontrado"
-fi
-
-
+#if [ ! -d "$arq" ] ; then
 #a=0
-#for i in "*.${ext[@]}"	; do
- # novo=$(printf "$1-%04d.${ext[$a]}" "$a") #04 pad to length of 4
-  #mv -v -- "$i" "$novo"
-  #echo "$novo"
-  #echo "$i"
-  #let a=a+1
+#for i in "$arq"; do
+#	echo "extensao ${i#*.} encontrada"
+#	let a++
 #done
+#else
+#	echo "Nenhum arquivo encontrado"
+#fi
+
+ext="AVI"
+
+a=0
+for i in *."$ext"; do
+  novo=$(printf "$1-%04d.avi" "$a") #04 pad to length of 4
+	echo "$i"
+	echo "$novo"
+  mv -v -- "$i" "$novo"
+  let a=a+1
+done
