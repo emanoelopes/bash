5902517159226a83a24f1f647b35484a87cacf2b
Emanoel Lopes <emanoel@ufc.br>
2015-01-14 15:31:56 -0300

    passando o nome e a extensao

--- renomeia.sh
+++ renomeia.sh
@@ -2,29 +2,48 @@
 
 #Renomear arquivos com o parametro $1 seguido de uma sequencia numerica e alterando a extensao informada no parametro $2
 
-
-function renomear() {
+function renomear {
 a=1
-for i in *."$2"; do
-  novo=$(printf "$1-%04d.$ext" "$a") #04 pad to length of 4
+for i in *."$opt"; do
+  novo=$(printf "$nome-%04d.$ext" "$a") #04 pad to length of 4
   mv -v -- "$i" "$novo"
   let a=a+1
-  echo "Arquivo existente"
+  echo $novo
 done
 }
 
-case $2 in 
-	[Jj][Pp][Gg])
-		ext="jpg"	
-		renomear
+nome=$1 #primeiro parametro será o nome comum a todos os arquivos.
+#o segundo parametro é a extensao, se nao for passado sera adotada a jpg/JPG
+if [ $2 ];then 
+opt=$2 
+else
+opt="jpg"
+fi
+
+echo $opt
+
+case $opt in
+	jpg|JPG)
+		echo "Renomeando imagens $opt"
+		ext="jpg"
+		renomear $nome $ext
 		;;
-	[Mm][Oo][Vv])
+	mov|MOV)
+		echo "Renomeando videos $opt"
 		ext="mov"
-		renomear
+		renomear $nome $ext
 		;;
-	[Aa][Vv][Ii])
+	avi|AVI)
+		echo "Renomeando videos $opt"
 		ext="avi"
-		renomear
+		renomear $nome $ext
+		;;
+	*)
+		echo "Nao foi informada uma extensao"
 		;;
 esac
 
+for f in *.JPG; do
+[ -e "*.JPG" ] && echo "existe $f" || echo "nao existe JPG!"
+
+done
