e607f050f94821b4b312bf8ba94f962d100b9fe7
Emanoel Lopes <emanoel@yahoo.com>
2015-01-14 11:31:33 -0300

    Melhorando com funcoes

--- renomeia.sh
+++ renomeia.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-#Renomear arquivos com o parametro $1 seguido de uma sequencia numerica.
+#Renomear arquivos com o parametro $1 seguido de uma sequencia numerica e alterando a extensao informada no parametro $2
 
-echo "digite a extensao de entrada"
-read ext1
-echo "digite a extensao de saida"
-read ext2
-a=0
-for i in *."$ext1"; do
-  novo=$(printf "$1-%04d.$ext2" "$a") #04 pad to length of 4
-	echo "$i"
-	echo "$novo"
+
+function renomear() {
+a=1
+for i in *."$2"; do
+  novo=$(printf "$1-%04d.$ext" "$a") #04 pad to length of 4
   mv -v -- "$i" "$novo"
   let a=a+1
+  echo "Arquivo existente"
 done
+}
+
+case $2 in 
+	[Jj][Pp][Gg])
+		ext="jpg"	
+		renomear
+		;;
+	[Mm][Oo][Vv])
+		ext="mov"
+		renomear
+		;;
+	[Aa][Vv][Ii])
+		ext="avi"
+		renomear
+		;;
+esac
+
